170ff886fa7aea2c7466b2d0c3a4c293c80c9a0f
SD-github21 <sapandonde.gmail.com>
2022-04-16 22:27:36 -0500

    add code to save events

--- script.js
+++ script.js
@@ -3,3 +3,23 @@ let currentDay = moment().format("dddd, MMMM Do YYYY");
 document.getElementById("currentDay").innerHTML = currentDay;
 
 // Add background styling dependent on time of day
+var events = {};
+
+// Save button is clicked
+$("#calendar .saveBtn").click(function() {
+    // Get values for event and hour
+    var eventText = $("#eventDescription").val();
+    var eventHour = $("#eventHour").val();
+
+        // Save in events array
+        events.push({
+            event: eventText,
+            hour: eventHour
+          });
+        });
+
+var saveEvents = function() {
+    localStorage.setItem("events", JSON.stringify(events));
+};
+          
+  
