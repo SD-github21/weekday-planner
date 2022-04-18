77baedb666e35e0c7082055092dbdb714cd7fe1a
SD-github21 <sapandonde.gmail.com>
2022-04-18 01:10:42 -0500

    latest code to try an each function

--- script.js
+++ script.js
@@ -102,35 +102,44 @@ var events = [];
 
 // Save button is clicked
 
-$("#9am").on("click", "button", (function() {
-    var eventText = $("#9amText").val();
-    console.log(eventText);
-    // Save in events array
-    events.push(eventText);
-    console.log(events);
-    localStorage.setItem("events", JSON.stringify(events));
-     }));
+// Successfully coded for each timeblock
+// $("#9am").on("click", "button", (function() {
+//     var eventText = $("#9amText").val();
+//     console.log(eventText);
+//     // Save in events array
+//     events.push(eventText);
+//     console.log(events);
+//     localStorage.setItem("events", JSON.stringify(events));
+//      }));
    
-$("#10am").on("click", "button", (function() {
-    var eventText = $("#10amText").val();
-    console.log(eventText);
+// $("#10am").on("click", "button", (function() {
+//     var eventText = $("#10amText").val();
+//     console.log(eventText);
+//     // Save in events array
+//     events.push(eventText);
+//     console.log(events);
+//     localStorage.setItem("events", JSON.stringify(events));
+//     }));
+
+
+// Attempts to consolidate above code into a more "DRY" function
+// $each(".row").on("click", "button", function(id) {
+//     var eventText = $("id").val();
+//     console.log(eventText);
+//     // Save in events array
+//     events.push(eventText);
+//     console.log(events);
+//     localStorage.setItem("events", JSON.stringify(events));
+// }));
+
+$(".row").each(function() {
+    $(".saveBtn").on("click", function(){
+        var eventText = $("textarea").val();
+        console.log(eventText); 
     // Save in events array
     events.push(eventText);
     console.log(events);
     localStorage.setItem("events", JSON.stringify(events));
-    }));
+    })
 
-
-
-$(".row").on("click", "button", function() {
-    $("textarea").each(function(id) {
-        var eventText = $("textarea"[id]).val();
-        console.log(eventText);
-            // Save in events array
-            events.push(eventText);
-            console.log(events);
-            localStorage.setItem("events", JSON.stringify(events));
-
-
-    })});
-    
+  });
